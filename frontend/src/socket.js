1d4ba756bf47687a8932f78b3242e579d8ffec9a
Chamath Milleniya <chamathmilleniya@Chamaths-MBP.modem>
2025-04-07 18:36:55 +1000

    Removed scaffolding frontend app

--- frontend/src/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8001';    // Server address
-
-export const socket = io(URL);
-
-// export const socket = io(URL, {
-//     autoConnect: false
-// });
